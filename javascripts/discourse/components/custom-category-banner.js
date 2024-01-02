c632b89019114e6925075e654c051e4754f72457
Lux de Lumine <lux@Hephaestus.local>
2024-01-02 14:29:13 -0500

    Complete revert

--- javascripts/discourse/components/custom-category-banner.js
+++ javascripts/discourse/components/custom-category-banner.js
@@ -1,9 +1,6 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
-
-// const schemeType = getComputedStyle(document.body)
-// .getPropertyValue("--scheme-type")
-// .trim();
+import { htmlSafe } from "@ember/template";
 
 export default class CustomCategoryBanner extends Component {
   @service router;
@@ -17,28 +14,11 @@ export default class CustomCategoryBanner extends Component {
   }
 
   get bannerBg() {
-    
     return htmlSafe(
       `background: url("${this.category.uploaded_background?.url}");
-     background-size: cover; 
-     background-position: center center;;`
+       background-size: cover; 
+       background-position: center center;;`
     );
-
-    // if (schemeType == "dark") {
-    //   return htmlSafe(
-    //     `background: url("${this.category.uploaded_background_dark?.url}");
-    //      background-size: cover; 
-    //      background-position: center center;;`
-    //   );
-
-    // }
-    // else {
-    //   return htmlSafe(
-    //     `background: url("${this.category.uploaded_background?.url}");
-    //    background-size: cover; 
-    //    background-position: center center;;`
-    //   );
-    // }
   }
 
   get categoryBgColor() {
